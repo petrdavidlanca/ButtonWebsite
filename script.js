9c64a0d1ae8c40185959a349d6f821ebbadbefb3
PetrDavidLanca <petrdavidlanca@gmail.com>
2025-06-12 16:49:53 +0200

    deleted script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-const button = document.getElementById("satisfyingButton");
-let count = 0;
-button.textContent = count;
-
-button.addEventListener("click", () => {
-    count++;
-    button.textContent = count;
-});
